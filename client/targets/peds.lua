e64788629f5c50b3af57b0fcdd9e408077ccb17e
Case <90377400+CaseIRL@users.noreply.github.com>
2024-03-13 18:51:03 +0000

    Delete client directory

--- client/targets/peds.lua
+++
@@ -1,6 +0,0 @@
---- This script handles any default target settings pre defined within the target system.
--- @script client/targets/ped.lua
-
-if config.default_actions.peds then
-    targets.peds = {}
-end
--- client/targets/players.lua
+++
@@ -1,6 +0,0 @@
---- This script handles any default target settings pre defined within the target system.
--- @script client/targets/players.lua
-
-if config.default_actions.players then
-    targets.players = {}
-end
